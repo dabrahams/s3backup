49d326acc2c291bc992730c49c651a44e795643e
Dave Abrahams <dave@boostpro.com>
2009-02-16 10:41:38 -0500

    Make the thing work again (or at least begin a backup).

--- backup.py
+++ backup.py
@@ -3,12 +3,13 @@ import os
 import sys
 import re
 import shutil
+import pathutils
 
 from aws_secrets import AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, PUBKEY, BUCKET
 
 BIND_DIRS=["/boot"]
-LVM_VOLS=["hydra/root"]
-LVM_SNAPSHOT_SIZE="50G"
+LVM_VOLS=["raid6/root"]
+LVM_SNAPSHOT_SIZE="5G"
 
 class path(str):
     def __div__(self, suffix):
@@ -18,25 +19,97 @@ class path(str):
     
     def __add__(self, suffix):
         return path(str.__add__(self,suffix))
-    def __radd__(self, prefix):
-        return path(str.__radd__(self,suffix))
+    #def __radd__(self, prefix):
+    #    return path(prefix + str(self))
 
     def basename(self):
         return path(os.path.basename(self))
     def dirname(self):
         return path(os.path.dirname(self))
 
-HOME=path('/usr/local/backup')
+HOME=path('/usr/local/s3backup')
 MNT=HOME/'mnt'
 DUMPS=HOME/'dumps'
-AUX=HOME/aux
+AUX=HOME/'aux'
 ARCHIVE=HOME/'.archive';
 
-os.mkdir('/tmp/backup.lock')
+VERBOSE=False
+def system(cmd):
+    if VERBOSE:
+        print sys.argv[0]+':',cmd
+    err = os.system(cmd)
+    if err:
+        raise OSError, 'failed system command with code %s:\n%r' % (err,cmd)
 
+mount_re=re.compile('^[^ ]+ %s/(.*)(?: [^ ]+){4}\n' % re.escape(MNT), re.MULTILINE)
+def mounts():
+    return mount_re.findall( open('/etc/mtab').read() )
+
+def cleanup ():
+    # log('Snapshot usage report...')
+#    df
+#     for fs in md0 md1 md2 md3 md4; do
+#         log Filesystem: /dev/$fs
+#         ps -o uid,pid,ppid,tty,time,args -p "$(fuser /dev/$fs 2>/dev/null)" 2>/dev/null
+#     done
+
+    MOUNTS = mounts()
+    log('Unmounting backup directories %s...' % MOUNTS)
+    for m in MOUNTS:
+        system('umount "%s"' % (MNT/m))
+        os.rmdir(MNT/m)
+
+    log('Clearing LVM snapshots %s...' % LVM_VOLS)
+    for v in LVM_VOLS:
+        snap = '/dev'/ v+'.backup'
+        if os.path.exists(snap):
+            system('/sbin/lvremove -f "%s"' % v+'.backup')
+
+    def clean(path):
+        if (os.path.exists(path)):
+            log('Cleaning up existing %s tree...' % os.path.split(path)[1])
+            shutil.rmtree(path, onerror=pathutils.onerror)
+
+    clean(MNT)
+    clean(AUX)
+    clean(DUMPS)
+
+def duplicity(source_dir, target_dir, opts='', verbose=VERBOSE and '-v4' or ''):
+    archive_dir = ARCHIVE/target_dir
+    demand_dirs(archive_dir)
+    pubkey = PUBKEY
+    uri = 's3+http://' + BUCKET + '/' + target_dir
+    system(
+        """duplicity --encrypt-key %(pubkey)s \
+        --archive-dir %(archive_dir)r \
+        %(verbose)s \
+        %(opts)s \
+        %(source_dir)r \
+        %(uri)r"""
+        % locals())
+
+def backup_mount (m, opts=''):
+    duplicity(
+        MNT/m, 
+        'mnt'/path(m), 
+        '--exclude-filelist %r --exclude-other-filesystems'  % (AUX/m/'exclude.txt')
+        )
+
+def log(*args):
+    if VERBOSE:
+        for x in args:
+            print x,
+        print
+
+def demand_dirs(path):
+    try:
+        os.makedirs(path)
+    except OSError, e:
+        if e.errno != 17: raise # ignore 'directory exists' errors
+
+os.mkdir('/var/lock/s3backup')
 try:
     # Process arguments
-    VERBOSE=False
     for o in sys.argv[1:]:
         if o in ('-v','--verbose'):
             VERBOSE=True
@@ -44,150 +117,94 @@ try:
             sys.stderr.write("usage: %s [-v]" % sys.argv[0])
             exit(1)
 
-    def log(*args):
-        if VERBOSE:
-            for x in args:
-                print x,
-            print
-
     BIND_DIRS=[path(x) for x in BIND_DIRS]
     LVM_VOLS=[path(x) for x in LVM_VOLS]
 
     # Set the umask so that all files are only readable by root
     os.umask(077)
 
-    def system(cmd):
-        if VERBOSE:
-            print sys.argv[0]+':',cmd
-        err = os.system(cmd)
-        if err:
-            raise OSError, 'failed system command with code %s:\n%r' % (err,cmd)
-
-    mount_re=re.compile('^[^ ]+ %s/(.*)(?: [^ ]+){4}\n' % re.escape(MNT))
-    def mounts():
-        return mount_re.finditer( open('/etc/mtab').read(), re.MULTILINE )
-
-    def cleanup ():
-        # log('Snapshot usage report...')
-    #    df
-    #     for fs in md0 md1 md2 md3 md4; do
-    #         log Filesystem: /dev/$fs
-    #         ps -o uid,pid,ppid,tty,time,args -p "$(fuser /dev/$fs 2>/dev/null)" 2>/dev/null
-    #     done
-
-        log('Unmounting backup directories...')
-        for m in mounts():
-            system('umount "%s"' % (MNT/m))
-
-        log('Clearing LVM snapshots...')
-        for v in LVM_VOLS:
-            snap = '/dev'/ v+'.backup'
-            if os.path.isdir(snap):
-                system('/sbin/lvremove -f "%s"' % v+'.backup')
-
     cleanup()
 
-    # create all snapshots as close together in time as possible
-    log('Taking snapshots...')
-    for v in LVM_VOLS:
-        lvname=v.basename()+'.backup'
-        system('/sbin/lvcreate -s -L %(LVM_SNAPSHOT_SIZE)r -n %(lvname)r %(v)r' % locals())
+    try:
+        # create all snapshots as close together in time as possible
+        log('Taking snapshots...')
+        for v in LVM_VOLS:
+            lvname=v.basename()+'.backup'
+            system('/sbin/lvcreate -s -L %(LVM_SNAPSHOT_SIZE)r -n %(lvname)r %(v)r' % locals())
 
-    log('Mounting snapshots...')
-    for v in LVM_VOLS:
-        mountpoint = MNT/'lvm'/v
-        device = '/dev'/v+'.backup'
-        os.makedirs(mountpoint)
-        # the nouuid option is needed because all of my logical volumes are XFS
-        system('mount %(device)r %(mountpoint)r -onouuid,ro' % locals())
+        log('Mounting snapshots...')
+        for v in LVM_VOLS:
+            mountpoint = MNT/'lvm'/v
+            device = '/dev'/v+'.backup'
+            os.makedirs(mountpoint)
+            # the nouuid option is needed because all of my logical volumes are XFS
+            system('mount %(device)r %(mountpoint)r -onouuid,ro' % locals())
 
-    log('Binding un-snapshottable filesystems...')
-    for d in BIND_DIRS:
-        mountpoint = MNT/'bind'/d
-        os.makedirs(mountpoint)
-        system('mount --bind -oro %(d)r %(mountpoint)r' % locals())
+        log('Binding un-snapshottable filesystems...')
+        for d in BIND_DIRS:
+            mountpoint = MNT/'bind'/d
+            os.makedirs(mountpoint)
+            system('mount --bind -oro %(d)r %(mountpoint)r' % locals())
 
-    for dir in (DUMPS,AUX):
-        system('rm -rf %r' % dir)
-        os.makedirs(dir)
+        for dir in (DUMPS,AUX):
+            os.makedirs(dir)
 
-    MOUNTS=list(mounts())
+        MOUNTS=mounts()
 
-    # log Dumping databases...
-    # pg_dumpall -U pgsql > $HOME/dumps/postgresql.dump
+        # log Dumping databases...
+        # pg_dumpall -U pgsql > $HOME/dumps/postgresql.dump
 
-    # log Dumping subversion repositories...
-    # svnadmin hotcopy /usr/local/svn/main $HOME/dumps/subversion.main
+        # log Dumping subversion repositories...
+        # svnadmin hotcopy /usr/local/svn/main $HOME/dumps/subversion.main
 
-    shutil.copyfile(HOME/'backup.sh', AUX/'backup.sh')
+        shutil.copyfile(HOME/'backup.py', AUX/'backup.py')
 
-    log('Finding hard links...')
-    for m in MOUNTS:
-        os.makedirs(AUX/m)
+        log('Finding hard links...')
+        for m in MOUNTS:
+            os.makedirs(AUX/m)
 
-    exclusions=[x.rstrip('\n') for x in open(HOME/'exclude.txt')]
+        if os.path.exists(HOME/'exclude.txt'):
+            exclusions=[x.rstrip('\n') for x in open(HOME/'exclude.txt')]
+        else:
+            exclusions=[]
 
-    find_prune=' '.join(['-wholename %r -prune -o'%(MNT/x) for x in exclusions])
-    for m in MOUNTS:
-        mountpoint = MNT/m
-        linkfile = AUX/m/'links.txt'
-        system(
-            'find %(mountpoint)r %(find_prune)s -type f -links +1 -print0 | xargs -r -0 ls -li | sort > %(linkfile)r'
-            % locals()
-            )
+        find_prune=' '.join(['-wholename %r -prune -o'%(MNT/x) for x in exclusions])
+        for m in MOUNTS:
+            mountpoint = MNT/m
+            linkfile = AUX/m/'links.txt'
+            system(
+                'find %(mountpoint)r %(find_prune)s -type f -links +1 -print0 | xargs -r -0 ls -li | sort > %(linkfile)r'
+                % locals()
+                )
 
-    log('Creating exclude lists...')
-    inode_re = re.compile('^ *([^ ]+)(?: +[^ ]+){7} +(.*)$')
-    for m in MOUNTS:
-        mx = [MNT/x for x in exclusions if x.startswith(m+'/')]
-        linkfile = AUX/m/'links.txt'
+        log('Creating exclude lists...')
+        inode_re = re.compile('^ *([^ ]+)(?: +[^ ]+){7} +(.*)$')
+        for m in MOUNTS:
+            mx = [MNT/x for x in exclusions if x.startswith(m+'/')]
+            linkfile = AUX/m/'links.txt'
 
-        prev=None
-        for inode,pathname in inode_re.finditer(open(linkfile).read(), re.M):
-            if inode == prev:
-                mx.append(pathname)
-            prev = inode
+            prev=None
+            for inode,pathname in inode_re.finditer(open(linkfile).read(), re.M):
+                if inode == prev:
+                    mx.append(pathname)
+                prev = inode
 
-        open(AUX/m/'exclude.txt','w').write('\n'.join(mx))
+            open(AUX/m/'exclude.txt','w').write('\n'.join(mx))
 
-    log('Creating archive directories...')
-    for d in MOUNTS + ['aux', 'dumps']:
-        os.makedirs(ARCHIVE/d)
+        log('Creating archive directories...')
+        for d in MOUNTS + ['aux', 'dumps']:
+            demand_dirs(ARCHIVE/d)
 
-    os.environ['AWS_ACCESS_KEY_ID'] = AWS_ACCESS_KEY_ID
-    os.environ['AWS_SECRET_ACCESS_KEY'] = AWS_SECRET_ACCESS_KEY
+        os.environ['AWS_ACCESS_KEY_ID'] = AWS_ACCESS_KEY_ID
+        os.environ['AWS_SECRET_ACCESS_KEY'] = AWS_SECRET_ACCESS_KEY
 
-    VERBOSE_FLAG=VERBOSE and '-v4' or ''
+        for m in MOUNTS:
+            backup_mount(m)
 
-    def duplicity(source_dir, target_dir, opts=''):
-        archive_dir = ARCHIVE/target_dir
-        uri = 's3+http://' + BUCKET/target_dir
-        system(
-            """duplicity --encrypt-key %(PUBKEY)s \
-            --archive-dir %(archive_dir)r \
-            %(VERBOSE_FLAG)s \
-            %(opts)s \
-            %(source_dir)r \
-            %(uri)r"""
-            % locals())
-
-    def backup_mount (m, opts=''):
-        # Added --exclude-other-filesystems just in case.  Somehow our LVM
-        # snapshot of / contained all the files at the top level of the
-        # /zorak zfs pool!
-        duplicity(
-            MNT/m, 
-            'mnt'/m, 
-            '--exclude-filelist %r --exclude-other-filesystems'  % (AUX/m/'exclude.txt')
-            )
-
-    for m in MOUNTS:
-        backup_mount(m)
-
-    duplicity(DUMPS, 'dumps')
-    duplicity(AUX, 'aux')
-
+        duplicity(DUMPS, 'dumps')
+        duplicity(AUX, 'aux')
+    finally:
+        cleanup()
 finally:
-    cleanup
-    rm -rf /tmp/backup.lock
+    os.rmdir('/var/lock/s3backup')
 
